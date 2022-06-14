1fa7c2f1b741f785e1d34864a8f106f1aa7c7f72
Justin <justinleng_@hotmail.com>
2022-06-14 23:51:37 +0800

    [Clean up] Claims page

--- app/screens/claims/ClaimsPaymentScreen.tsx
+++ app/screens/claims/ClaimsPaymentScreen.tsx
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     backgroundColor: "transparent",
-    alignItems: "center",
+    alignItems: "flex-end",
     paddingHorizontal: 20,
   },
 });
@@ -33,178 +33,179 @@ const styles = StyleSheet.create({
 export default function ClaimsPaymentScreen({ navigation }: RootStackScreenProps<"ClaimsPayment">) {
   return (
     <View style={{ backgroundColor: "#FFE9E9", height: "100%" }}>
-      <View style={{ backgroundColor: "#FFE9E9", height: "100%", width: "100%" }}>
-        <View style={styles.container}>
-          <TouchableOpacity
+      <View style={styles.container}>
+        <TouchableOpacity
+          elevation={5}
+          style={{
+            backgroundColor: "#D8D8D8",
+            width: "50%",
+            height: 58,
+            borderRadius: 20,
+            marginVertical: 20,
+            borderColor: "#676767",
+            borderWidth: 2,
+          }}
+          onPress={() => navigation.navigate("SubmitClaims")}
+        >
+          <Text
             style={{
-              backgroundColor: "#D8D8D8",
-              width: 358,
-              height: 58,
-              borderRadius: 20,
-              marginVertical: 20,
+              fontSize: 15,
+              height: "100%",
+              textAlign: "center",
+              textAlignVertical: "center",
             }}
-            onPress={() => navigation.navigate("SubmitClaims")}
+          >
+            Submit self-employed claims
+          </Text>
+        </TouchableOpacity>
+        <View
+          elevation={5}
+          style={{
+            backgroundColor: "#FFFAFA",
+            width: "100%",
+            borderRadius: 20,
+            overflow: "hidden",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <View style={styles.filter}>
+            <Text style={{ fontSize: 17, fontWeight: "bold" }}>FILTER BY</Text>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                backgroundColor: "transparent",
+                width: "100%",
+                justifyContent: "space-between",
+                marginVertical: 5,
+              }}
+            >
+              <Text style={{ width: 45 }}>From:</Text>
+              <View style={styles.dropDown}>
+                <Text style={{ textAlign: "center", textAlignVertical: "center", width: "80%" }}>YYYY</Text>
+                <Image style={styles.chevron} source={{ uri: "https://i.imgur.com/HcspvOp.png" }} />
+              </View>
+              <View style={styles.dropDown}>
+                <Text style={{ textAlign: "center", textAlignVertical: "center", width: "80%" }}>MMMM</Text>
+                <Image style={styles.chevron} source={{ uri: "https://i.imgur.com/HcspvOp.png" }} />
+              </View>
+            </View>
+
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                backgroundColor: "transparent",
+                width: "100%",
+                justifyContent: "space-between",
+                marginVertical: 5,
+              }}
+            >
+              <Text style={{ width: 45 }}>To:</Text>
+              <View style={styles.dropDown}>
+                <Text style={{ textAlign: "center", textAlignVertical: "center", width: "80%" }}>YYYY</Text>
+                <Image style={styles.chevron} source={{ uri: "https://i.imgur.com/HcspvOp.png" }} />
+              </View>
+              <View style={styles.dropDown}>
+                <Text style={{ textAlign: "center", textAlignVertical: "center", width: "80%" }}>MMMM</Text>
+                <Image style={styles.chevron} source={{ uri: "https://i.imgur.com/HcspvOp.png" }} />
+              </View>
+            </View>
+          </View>
+
+          <View
+            style={{
+              backgroundColor: "#FFB5B5",
+              width: 329,
+              height: 30,
+              borderRadius: 20,
+              marginTop: 20,
+            }}
           >
             <Text
               style={{
-                fontSize: 25,
+                fontSize: 20,
                 height: "100%",
                 textAlign: "center",
                 textAlignVertical: "center",
               }}
             >
-              Submit self-employed claims
+              Payment History
             </Text>
-          </TouchableOpacity>
+          </View>
+
           <View
-            elevation={5}
             style={{
-              backgroundColor: "#FFFAFA",
-              width: "100%",
-              borderRadius: 20,
-              overflow: "hidden",
-              display: "flex",
-              alignItems: "center",
+              backgroundColor: "transparent",
+              paddingVertical: 20,
+              borderBottomWidth: 1,
             }}
           >
-            <View style={styles.filter}>
-              <Text style={{ fontSize: 17, fontWeight: "bold" }}>FILTER BY</Text>
-              <View
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  backgroundColor: "transparent",
-                  width: "100%",
-                  justifyContent: "space-between",
-                  marginVertical: 5,
-                }}
-              >
-                <Text style={{ width: 45 }}>From:</Text>
-                <View style={styles.dropDown}>
-                  <Text style={{ textAlign: "center", textAlignVertical: "center", width: "80%" }}>YYYY</Text>
-                  <Image style={styles.chevron} source={{ uri: "https://i.imgur.com/HcspvOp.png" }} />
-                </View>
-                <View style={styles.dropDown}>
-                  <Text style={{ textAlign: "center", textAlignVertical: "center", width: "80%" }}>MMMM</Text>
-                  <Image style={styles.chevron} source={{ uri: "https://i.imgur.com/HcspvOp.png" }} />
-                </View>
-              </View>
-
-              <View
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  backgroundColor: "transparent",
-                  width: "100%",
-                  justifyContent: "space-between",
-                  marginVertical: 5,
-                }}
-              >
-                <Text style={{ width: 45 }}>To:</Text>
-                <View style={styles.dropDown}>
-                  <Text style={{ textAlign: "center", textAlignVertical: "center", width: "80%" }}>YYYY</Text>
-                  <Image style={styles.chevron} source={{ uri: "https://i.imgur.com/HcspvOp.png" }} />
-                </View>
-                <View style={styles.dropDown}>
-                  <Text style={{ textAlign: "center", textAlignVertical: "center", width: "80%" }}>MMMM</Text>
-                  <Image style={styles.chevron} source={{ uri: "https://i.imgur.com/HcspvOp.png" }} />
-                </View>
-              </View>
-            </View>
-
-            <TouchableOpacity
+            <Text>{`IPPT (GIRO)\n`}</Text>
+            <Text>Start time: 03 July 2021, 1642 hrs</Text>
+            <Text>{`End time: 03 July 2021, 1800 hrs\n`}</Text>
+            <View
               style={{
-                backgroundColor: "#FFB5B5",
+                backgroundColor: "#D9D9D9",
                 width: 329,
-                height: 30,
-                borderRadius: 20,
-                marginTop: 20,
+                height: 50,
+                borderRadius: 10,
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                paddingHorizontal: 10,
+                paddingVertical: 10,
               }}
             >
-              <Text
-                style={{
-                  fontSize: 20,
-                  height: "100%",
-                  textAlign: "center",
-                  textAlignVertical: "center",
-                }}
-              >
-                Payment History
-              </Text>
-            </TouchableOpacity>
-
-            <View
-              style={{
-                backgroundColor: "transparent",
-                paddingVertical: 20,
-                borderBottomWidth: 1,
-              }}
-            >
-              <Text>{`IPPT (GIRO)\n`}</Text>
-              <Text>Start time: 03 July 2021, 1642 hrs</Text>
-              <Text>{`End time: 03 July 2021, 1800 hrs\n`}</Text>
-              <View
-                style={{
-                  backgroundColor: "#D9D9D9",
-                  width: 329,
-                  height: 50,
-                  borderRadius: 10,
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  paddingHorizontal: 10,
-                  paddingVertical: 10,
-                }}
-              >
-                <View style={{ backgroundColor: "transparent" }}>
-                  <Text>Service pay: S$19.76</Text>
-                  <Text>Make-up Pay: -</Text>
-                </View>
-                <Text>S$19.76</Text>
+              <View style={{ backgroundColor: "transparent" }}>
+                <Text>Service pay: S$19.76</Text>
+                <Text>Make-up Pay: -</Text>
               </View>
+              <Text>S$19.76</Text>
             </View>
+          </View>
 
+          <View
+            style={{
+              backgroundColor: "transparent",
+              paddingVertical: 20,
+              borderBottomWidth: 1,
+            }}
+          >
+            <Text>{`IPPT AWARD (GIRO)\n`}</Text>
+            <Text>Start time: 03 July 2021, 1642 hrs</Text>
+            <Text>{`End time: 03 July 2021, 1800 hrs\n`}</Text>
             <View
               style={{
-                backgroundColor: "transparent",
-                paddingVertical: 20,
-                borderBottomWidth: 1,
+                backgroundColor: "#D9D9D9",
+                width: 329,
+                height: 50,
+                borderRadius: 10,
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                paddingHorizontal: 10,
+                paddingVertical: 10,
               }}
             >
-              <Text>{`IPPT AWARD (GIRO)\n`}</Text>
-              <Text>Start time: 03 July 2021, 1642 hrs</Text>
-              <Text>{`End time: 03 July 2021, 1800 hrs\n`}</Text>
-              <View
-                style={{
-                  backgroundColor: "#D9D9D9",
-                  width: 329,
-                  height: 50,
-                  borderRadius: 10,
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  paddingHorizontal: 10,
-                  paddingVertical: 10,
-                }}
-              >
-                <View style={{ backgroundColor: "transparent" }}>
-                  <Text>Service pay: -</Text>
-                  <Text>Make-up Pay: -</Text>
-                </View>
-                <Text>S$300.00</Text>
+              <View style={{ backgroundColor: "transparent" }}>
+                <Text>Service pay: -</Text>
+                <Text>Make-up Pay: -</Text>
               </View>
+              <Text>S$300.00</Text>
             </View>
-            <View
-              style={{
-                backgroundColor: "transparent",
-                width: "100%",
-                padding: 20,
-              }}
-            >
-              <Text style={{ textAlign: "center", fontSize: 17 }}> - No other transactions -</Text>
-            </View>
+          </View>
+          <View
+            style={{
+              backgroundColor: "transparent",
+              width: "100%",
+              padding: 20,
+            }}
+          >
+            <Text style={{ textAlign: "center", fontSize: 17 }}> - No other transactions -</Text>
           </View>
         </View>
       </View>
